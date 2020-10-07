646ee7844ca20ad00aa223b280831409a8ca0ca7
antobrines <antonin.brines@outlook.fr>
2020-10-07 16:34:32 +0200

    Ex202 test Pass

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -1,8 +1,19 @@
 package com.ipiecoles.java.java220;
 
 public class Commercial extends Employe{
+
+    private Double caAnnuel = 0d;
+
     @Override
     public Double getPrimeAnnuelle() {
         return null;
+    }
+
+    public Double getCaAnnuel() {
+        return caAnnuel;
+    }
+
+    public void setCaAnnuel(Double caAnnuel) {
+        this.caAnnuel = caAnnuel;
     }
 }
