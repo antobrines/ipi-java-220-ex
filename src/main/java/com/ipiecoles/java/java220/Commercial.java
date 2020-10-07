f15b9c948b96d41dcfc8aab1d1a2945dd3255ed4
antobrines <antonin.brines@outlook.fr>
2020-10-07 17:04:27 +0200

    Ex204 test Pass, https://pjvilloud.github.io/ipi-java-220-cours/#/heritage-abstraites

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -1,8 +1,19 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 public class Commercial extends Employe{
 
     private Double caAnnuel = 0d;
+
+    public Commercial() {
+
+    }
+
+    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel) {
+        super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.caAnnuel = caAnnuel;
+    }
 
     @Override
     public Double getPrimeAnnuelle() {
