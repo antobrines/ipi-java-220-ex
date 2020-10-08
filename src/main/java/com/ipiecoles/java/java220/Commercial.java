a8310017547cb5c2115ff0dbc5f16fe7ea2a76eb
antobrines <antonin.brines@outlook.fr>
2020-10-08 08:21:29 +0200

    Ex205 test Pass, generate

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -2,6 +2,8 @@ package com.ipiecoles.java.java220;
 
 import org.joda.time.LocalDate;
 
+import java.util.Objects;
+
 public class Commercial extends Employe{
 
     private Double caAnnuel = 0d;
@@ -13,6 +15,15 @@ public class Commercial extends Employe{
     public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel) {
         super(nom, prenom, matricule, dateEmbauche, salaire);
         this.caAnnuel = caAnnuel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Commercial)) return false;
+        if (!super.equals(o)) return false;
+        Commercial that = (Commercial) o;
+        return Objects.equals(getCaAnnuel(), that.getCaAnnuel());
     }
 
     @Override
