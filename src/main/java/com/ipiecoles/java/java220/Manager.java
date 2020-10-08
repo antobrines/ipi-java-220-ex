c9b9ad50533cd67cb8d310b0ee94d932b68220a4
antobrines <antonin.brines@outlook.fr>
2020-10-08 13:11:22 +0200

    Ex502 test Pass, https://beginnersbook.com/2013/12/hashset-class-in-java-with-example/

--- src/main/java/com/ipiecoles/java/java220/Manager.java
+++ src/main/java/com/ipiecoles/java/java220/Manager.java
@@ -1,10 +1,21 @@
 package com.ipiecoles.java.java220;
 
+import java.util.HashSet;
+
 public class Manager extends Employe{
 
+    private HashSet<Technicien> equipe = new HashSet<Technicien>();
 
     @Override
     public Double getPrimeAnnuelle() {
         return null;
+    }
+
+    public HashSet<Technicien> getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(HashSet<Technicien> equipe) {
+        this.equipe = equipe;
     }
 }
