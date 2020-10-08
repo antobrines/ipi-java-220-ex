20146e3bcca98e9de06de81b050ac7d88d21fd34
antobrines <antonin.brines@outlook.fr>
2020-10-08 10:04:16 +0200

    Ex302 test Pass

--- src/main/java/com/ipiecoles/java/java220/Technicien.java
+++ src/main/java/com/ipiecoles/java/java220/Technicien.java
@@ -2,7 +2,17 @@ package com.ipiecoles.java.java220;
 
 public class Technicien extends Employe{
 
+    private Integer grade = 0;
+
     public Technicien(){
 
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
     }
 }
