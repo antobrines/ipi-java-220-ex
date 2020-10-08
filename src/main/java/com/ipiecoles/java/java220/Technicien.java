e89125adee3aba4e239a8ac222729fa95f52b205
antobrines <antonin.brines@outlook.fr>
2020-10-08 10:05:45 +0200

    Ex303 test Pass, generate

--- src/main/java/com/ipiecoles/java/java220/Technicien.java
+++ src/main/java/com/ipiecoles/java/java220/Technicien.java
@@ -1,11 +1,18 @@
 package com.ipiecoles.java.java220;
 
+import org.joda.time.LocalDate;
+
 public class Technicien extends Employe{
 
     private Integer grade = 0;
 
     public Technicien(){
 
+    }
+
+    public Technicien(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Integer grade) {
+        super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.grade = grade;
     }
 
     public Integer getGrade() {
